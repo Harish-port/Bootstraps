4af7018150e10744fa93a34650ea5e7c867401a3
Harish-port <harish.sq@gmail.com>
2021-05-04 09:19:05 +0530

    added few things

--- javascript/Undefined_Null/undefinedNull.js
+++ javascript/Undefined_Null/undefinedNull.js
@@ -2,3 +2,13 @@
 
 let name;  // undefined
 let age = null;   //null
+
+console.log(typeof null);///object
+console.log(typeof undefined);///undefined
+
+
+console.log(undefined === null);///false
+console.log( undefined == null);///true
+
+/// === is called as strict equality
+/// == is called abstract equality
