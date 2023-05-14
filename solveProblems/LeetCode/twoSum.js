a9cde35f27712642ed55e46ca001386a74ff6ba2
Harish-port <harish.sq@gmail.com>
2023-05-14 06:12:58 +0530

    Two sum

--- solveProblems/LeetCode/twoSum.js
+++ solveProblems/LeetCode/twoSum.js
@@ -4,7 +4,6 @@
 
 // You can return the answer in any order.
 
-<<<<<<< HEAD
 // Input: nums = [2,7,11,15], target = 9
 // Output: [0,1]
 // Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
@@ -21,17 +20,4 @@ var twoSum = (nums, target) => {
 }
 
 twoSum([2,7,11,15],9)
-=======
-var nums= [2,7,11,15],target = 9;
 
-var twoSum = function(nums, target) {
- for(let i = 0;i<nums.length-1;i++){
-     for(let j=i+1;j<nums.length-1;j++){
-         if(nums[i]+nums[j]===target){
-             return [i,j]
-         }
-     }
- }
-}
-twoSum(nums,target)
->>>>>>> f76838575859458d4f7300d0f5e629875e19cf92
