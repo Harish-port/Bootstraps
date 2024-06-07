915a7b5ec87f228abb8d3ad69cce09119120dd32
Harish S <harish.sq@gmail.com>
2024-06-07 07:48:22 +0530

    flatten obj

--- solveProblems/Companies/FlattenNestedObj.js
+++ solveProblems/Companies/FlattenNestedObj.js
@@ -1,4 +1,5 @@
 // for the given user object flatten into a simpler one for example like
+// output 1
 //    {
 //     user_name: "Rowdy Coders",
 //     user_address_primary_house: "109",
@@ -22,14 +23,47 @@ const user = {
   },
 };
 
-const flattenObj = (obj) => {
-  const result = {};
-  for(key in obj){
-    if(typeof obj[key] === 'obj'){
-      
+function flattenObject(obj, prefix = "") {
+  let flattened = {};
+  for (let key in obj) {
+    if (typeof obj[key] === "object" && !Array.isArray(obj[key])) {
+      flattened = {
+        ...flattened,
+        ...flattenObject(obj[key], `${prefix}${key}_`),
+      };
+    } else {
+      flattened[`${prefix}${key}`] = obj[key];
+    }
+  }
+  return flattened;
+} // Flatten the user object with "user_" prefix
+
+const flattenedUser = flattenObject(user, "user_");
+console.log(flattenedUser);
+const flattenObj = (obj, parentKey) => {
+  let result = {};
+  for (key in obj) {
+    if (typeof obj[key] === "object" && !Array.isArray(obj[key])) {
+      result = {
+        ...result,
+        ...flattenObj(
+          obj[key],
+          `${parentKey}${key.charAt(0).toUpperCase() + key.slice(1)}`
+        ),
+      };
+    } else {
+      result[`${parentKey}${key.charAt(0).toUpperCase() + key.slice(1)}`] =
+        obj[key];
     }
   }
   return result;
 };
 
-console.log(flattenObj(user), "result");
+console.log(flattenObj(user, "user"), "result");
+// // output 2
+// {
+//   userName: "Rowdy Coders",
+//   userAddressPrimaryHouse: "109",
+//   userAddressPrimaryStreetMain: 21,
+//   userAddressPrimaryStreetCross: ["32", "1"],
+//   }
