2ab594a80f756dd4b388031792491ee47286d95a
Harish S <harish.sq@gmail.com>
2024-06-08 21:15:25 +0530

    changes two sum

--- solveProblems/LeetCode/twoSum.js
+++ solveProblems/LeetCode/twoSum.js
@@ -8,16 +8,32 @@
 // Output: [0,1]
 // Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
 
-
 var twoSum = (nums, target) => {
-    for (let i = 0; i <= nums.length; i++){
-        for(let j = i+1; j <= nums.length; j++){
-            if(nums[i] + nums[j] === target){
-                return [i,j];
-            }
-        }
+  for (let i = 0; i <= nums.length; i++) {
+    for (let j = i + 1; j <= nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
+      }
     }
-}
+  }
+};
 
-twoSum([2,7,11,15],9)
+twoSum([2, 7, 11, 15], 9);
 
+// Space - O(1)
+// Time - O(N)^2
+
+// Optimized solutuion
+
+var twoSum2 = (nums, target) => {
+  let memory = {};
+  for (let i = 0; i <= nums.length; i++) {
+    if (memory[nums[i]] === undefined) {
+      memery[target - nums[i]] = i;
+    } else {
+      return [memery[(nums[i], i)]];
+    }
+  }
+};
+
+twoSum2([2, 7, 11, 15], 9);
