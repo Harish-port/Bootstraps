b9da60699982fec73442607142e70207265bf477
Harish S <harish.sq@gmail.com>
2024-08-13 17:00:38 +0530

    ADded

--- solveProblems/polyfills/Memoize.js
+++ solveProblems/polyfills/Memoize.js
@@ -1,16 +1,34 @@
 // If the paramaters of functions are same then we need to cahce the result of the previous function somewhere
-function myMemoize(fn) {
-    
+function myMemoize(fn, context) {
+  const res = {};
+  return function (...args) {
+    var argsCache = JSON.stringify(args);
+    if (!res[argsCache]) {
+      res[argsCache] = fn.call(context || this, ...args);
+    }
+    return res[argsCache];
+  };
 }
-const clumsySquare = (num1, num2) => {
+const clumsyProduct = (num1, num2) => {
   for (let i = 1; i <= 1000000000; i++) {}
   return num1 + num2;
 };
+// withoutmemoized function
+// console.time("First Call");
+// console.log(clumsyProduct(9399, 7343));
+// console.timeEnd("First Call");
 
+// console.time("Second Call");
+// console.log(clumsyProduct(9399, 7343));
+// console.timeEnd("Second Call");
+
+// with memoized function
+
+const memoizedClumzyPRoduct = myMemoize(clumsyProduct);
 console.time("First Call");
-console.log(clumsySquare(9399, 7343));
+console.log(memoizedClumzyPRoduct(9399, 7343));
 console.timeEnd("First Call");
 
 console.time("Second Call");
-console.log(clumsySquare(9399, 7343));
+console.log(memoizedClumzyPRoduct(9399, 7343));
 console.timeEnd("Second Call");
