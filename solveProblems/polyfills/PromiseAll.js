541828f0004dd9ef386aefe50b4e2ff6503d5179
Harish S <harish.sq@gmail.com>
2024-08-17 12:56:42 +0530

    promiseAll polyfill

---
+++ solveProblems/polyfills/PromiseAll.js
@@ -0,0 +1,57 @@
+// Promise.All in js
+
+function importantAction(userName) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      reject(`Subscribe to ${userName}`);
+    }, 1000);
+  });
+}
+
+function likeTheVideo(video) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(`Like the ${video} video`);
+    }, 1000);
+  });
+}
+
+function shareTheVideo(video) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(`Share the ${video} video`);
+    }, 1000);
+  });
+}
+
+Promise.allPolyFill = (promises) => {
+  return new Promise((resolve, reject) => {
+    const results = [];
+    if (!promises.length) {
+      resolve(results);
+      return;
+    }
+    let pending = promises.length;
+    promises.forEach((promise, idx) => {
+      Promise.resolve(promise).then((res) => {
+        results[idx] = res;
+        pending--;
+        if (pending === 0) {
+          resolve(results);
+        }
+      }, reject);
+    });
+  });
+};
+
+Promise.allPolyFill([
+  importantAction("youtube channel"),
+  likeTheVideo("JS interview Questions"),
+  shareTheVideo("JS interview Questions"),
+])
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
