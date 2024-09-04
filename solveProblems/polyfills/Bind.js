7713ac6c814e9c1277667cdf22ce535d32a29e88
Harish S <harish.sq@gmail.com>
2024-09-04 22:43:29 +0530

    bind method explaination

--- solveProblems/polyfills/Bind.js
+++ solveProblems/polyfills/Bind.js
@@ -11,7 +11,11 @@ function purchaseCar(currency, price) {
     `I have purchased ${this.color} - ${this.company} car for ${currency} ${price}`
   );
 }
-
+// Steps to remember
+// 1. check for edge cases that is check if this is a function or not
+// 2. assign this to context object since purchaseCar is function here
+// 3. so that any this keyowrd inside purchasecar will point to context object
+// 4. since bind method return a function which is not invoked immediately, create a annoynomus function with newArgs and spread it
 Function.prototype.myBind = function (context = {}, ...args) {
   if (typeof this !== "function") {
     throw new Error(this + "It's not callable");
