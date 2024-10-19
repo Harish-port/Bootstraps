124a5fa076555dea329861d45c1c617b0919f2ca
Harish S <harish.sq@gmail.com>
2024-10-19 23:16:16 +0530

    today's changes

--- javascript/MachineCodingRound/EmployeeDataBaseManagement/script.js
+++ javascript/MachineCodingRound/EmployeeDataBaseManagement/script.js
@@ -5,12 +5,37 @@
   let selectedEmployeeId = employees[0].id;
   let selectedEmployee = employees[0];
   const displayEmployeeList = document.getElementById("render-employee-list");
+  const addEmployeeDetails = document.querySelector(".add-employee");
+  const createEmployeeModal = document.querySelector(".add-employee-modal");
+  const formSubmitData = document.querySelector(".add-employee-create");
   displayEmployeeList.addEventListener("click", (e) => {
     if (e.target.tagName === "LI" && selectedEmployeeId !== e.target.id) {
       selectedEmployeeId = e.target.id;
     }
     renderEmployeeList();
     renderEmployeeInformation();
+  });
+  formSubmitData.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const formData = new FormData(formSubmitData);
+    const values = [...formData.entries()];
+    let employeeData = {};
+    values.forEach((val) => {
+      employeeData[val[0]] = val[1];
+    });
+    employeeData.id = employees[employees.length - 1].id + 1;
+    employees.push(employeeData);
+    renderEmployeeList();
+    formSubmitData.reset();
+    createEmployeeModal.style.display = "none";
+  });
+  addEmployeeDetails.addEventListener("click", () => {
+    createEmployeeModal.style.display = "flex";
+  });
+  createEmployeeModal.addEventListener("click", (e) => {
+    if (e.target.className === "add-employee-modal") {
+      createEmployeeModal.style.display = "none";
+    }
   });
 
   const renderEmployeeList = () => {
@@ -28,11 +53,14 @@
       displayEmployeeList.append(li);
     });
   };
+
   const renderEmployeeInformation = () => {
     if (selectedEmployeeId) {
       const displayEmployeeInfo = document.querySelector(
         ".employee-information-details"
       );
+      console.log(selectedEmployee, "selectedEmployee");
+
       displayEmployeeInfo.innerHTML = "";
       const divElement = document.createElement("div");
       const { address, email, contactNumber, firstName, lastName, dob } =
