a017de04e24c8d768100494c68a9251272f0450d
Harish S <harish.sq@gmail.com>
2024-12-11 00:48:45 +0530

    2 leetcodes questions added

--- solveProblems/LeetCode/Easy/Strings/ValidAnagram.js
+++ solveProblems/LeetCode/Easy/Strings/ValidAnagram.js
@@ -1,31 +1,40 @@
+// with string methods
+
+function isAnagram1(s, t) {
+  if (s.length !== t.length) {
+    return false;
+  }
+  return s.split("").sort().join("") === t.split("").sort().join("");
+}
+// without string methods
 function isAnagram(s, t) {
-    // If lengths are different, they cannot be anagrams
-    if (s.length !== t.length) {
-        return false;
-    }
+  // If lengths are different, they cannot be anagrams
+  if (s.length !== t.length) {
+    return false;
+  }
 
-    // Create frequency maps for both strings
-    const frequencyS = {};
-    const frequencyT = {};
+  // Create frequency maps for both strings
+  const frequencyS = {};
+  const frequencyT = {};
 
-    // Populate frequency maps
-    for (let char of s) {
-        frequencyS[char] = (frequencyS[char] || 0) + 1;
-    }
-    for (let char of t) {
-        frequencyT[char] = (frequencyT[char] || 0) + 1;
-    }
+  // Populate frequency maps
+  for (let char of s) {
+    frequencyS[char] = (frequencyS[char] || 0) + 1;
+  }
+  for (let char of t) {
+    frequencyT[char] = (frequencyT[char] || 0) + 1;
+  }
 
-    // Compare frequencies
-    for (let char in frequencyS) {
-        if (frequencyS[char] !== frequencyT[char]) {
-            return false;
-        }
+  // Compare frequencies
+  for (let char in frequencyS) {
+    if (frequencyS[char] !== frequencyT[char]) {
+      return false;
     }
+  }
 
-    return true;
+  return true;
 }
 
 // Example usage
-console.log(isAnagram("racecar", "carrace")); // Output: true
-console.log(isAnagram("jar", "jam"));        // Output: false
+console.log(isAnagram1("racecar", "carrace")); // Output: true
+console.log(isAnagram1("jar", "jam")); // Output: false
