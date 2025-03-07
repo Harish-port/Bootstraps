54bf2cc96d4a6784caf83454116d8afded0b81ea
Harish S <harish.sq@gmail.com>
2025-03-07 18:03:44 +0530

    mongodb

--- Databases/mongoDB/mongoDB.js
+++ Databases/mongoDB/mongoDB.js
@@ -1,1 +1,52 @@
-1. What is MongoDB?
+// 1. What is MongoDB?
+//  It is a NoSQL database that stores data in JSON-like documents
+
+// 2. What is Mongoose?
+// It is a Object data Modelling(ODM) library for MongoDB and Nodejs.It provides a structured way to interact with MongoDB by defining schemas and using models to perform CRUD operations.
+// >Think of it like this:
+// . MongoDB is a NoSQL database (stores JSON-like data)
+// . Mongoose helps you define a structure (schema) for that data and interact with it in an organized way.
+
+//3. what is NoSQL and SQL database and what is the difference between them?
+
+// SQL(Structed Query Language) databases are relational and uses table to store data with predefined schemas
+// NoSQL(Not only SQL) databases are non-relational, offering flexible schemas and stores data in JSON-like document
+
+// 1.Data Model
+// > SQL database stores data in tables(row & columns)
+// > NoSQL databases store data in documents(JSON-like), key-value PerformanceObserverEntryList
+
+// 2. Schema
+// > SQL databases have a fixed schema(you must define tables and columns before inserting data).
+// > NoSQL databases are schema-less(each document can have different fields).
+
+// 3. Scalability
+// > SQL - Vertical Scaling(Increase CPU,RAM,SSD on a single server).
+// > NoSQL - Horizontal Scaling(Distribute data across multiple servers, making it highly scalable)
+
+// Example:
+// A banking system (SQL) may need strong consistency(transactions must be accurate)|
+// A social media app(NoSQL) needs fast performance and scalability (handling millions of users posting images)
+
+// 4. Performance and Querying 
+// SQL is optimized for complex queries (JOIN, GROUP BY, HAVING).
+// NoSQL is optimized for fast read & write operations.
+
+// 5️⃣ Transactions & Consistency
+// SQL databases use ACID (Atomicity, Consistency, Isolation, Durability) → Ensures strong consistency.
+// NoSQL databases use BASE (Basically Available, Soft state, Eventually consistent) → Optimized for speed, slight delay in consistency.
+// 📌 Example:
+
+// SQL is better for Banking (accurate transactions needed)
+// NoSQL is better for Social Media (speed is more important than strict accuracy)
+
+// Which One to Choose?
+// ✔ Choose SQL if:
+// ✅ You need structured data (Banking, ERP, CRM)
+// ✅ You require strong transactions (Financial systems)
+// ✅ You perform complex queries & joins
+
+// ✔ Choose NoSQL if:
+// ✅ You have unstructured or semi-structured data
+// ✅ You need high scalability (Social Media, Big Data, IoT)
+// ✅ You work with rapidly changing requirements (Startups, Dynamic Apps)
---
+++ Databases/mongoDB/mongooseUsage.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://127.0.0.1:27017/mydatabase",{
+    useNewUrlParser:true,useUnifiedTopology:true
+}).then(()=>console.log("MongoDB connection success")
+).catch((err)=>console.log("Connection error",err));
+
+//Define Schema
+// A schema defines the structure of documents in a MongoDB collection
+
+// User Schema Example
+
+const userSchema = new mongoose.Schema({
+    name:String,
+    age:Number,
+    email:{type:String,required:true,unique:true}
+});
+
+// Create A Model
+// A model is a javascript representation of MongoDB Collection based on Schema
+
+const User = mongoose.model("User",userSchema);
+
+// Insert Data 
+const newUser = new User({
+    name:"Harish",
+    age:28,
+    email:"harish@gmail.com"
+});
+
+newUser.save().then(()=>console.log("User Saved")).catch(err=>console.error("Error",err));
+
+// Fetch Data
+
+User.find().then(users => console.log(users)).catch(err => console.error(err));
+
+// Update Data
+
+User.updateOne({name:"Harish"},{age:25}).then(()=>console.log("User Updated")).catch(err => console.log(err));
+
+// Delete Data 
+
+User.deleteOne({name:"Harish"}).then(()=>console.log("User Deleted")).catch(err => console.log(err));
+
